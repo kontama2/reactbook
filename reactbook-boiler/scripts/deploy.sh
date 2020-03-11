691ad8bc984b272584649bf966f058540d241c17
kontama2 <kontama2p@gmail.com>
2020-03-11 20:10:55 +0900

    Read chapter-5

---
+++ reactbook-boiler/scripts/deploy.sh
@@ -0,0 +1,17 @@
+# 以前のバージョンのクリーンアップ
+rm -rf __deployme
+mkdir __deployme
+
+# ビルド
+sh scripts/build.sh
+
+# JavaScript のミニファイ
+uglify -s bundle.js -o __deployme/bundle.js
+# CSS のミニファイ
+cssshrink bundle.css > __deployme/bundle.css
+# HTML と画像のコピー
+cp index.html __deployme/index.html
+cp -r images/ __deployme/images/
+
+# 完了
+date; echo;
